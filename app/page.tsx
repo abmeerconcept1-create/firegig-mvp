626ce4f0e617feca0cc316d93ce0392caeae6545
abmeerconcept1-create <abmeerconcept1@gmail.com>
2026-06-12 09:23:27 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 20, fontFamily: "Arial" }}>
+      <h1>🔥 FIREGIG MVP</h1>
+
+      <p>Earn money by completing simple online tasks</p>
+
+      <div style={{ marginTop: 20 }}>
+        <a href="/jobs">👉 View Jobs</a>
+        <br />
+        <a href="/wallet">💰 Wallet</a>
+      </div>
+    </main>
+  );
+      }
